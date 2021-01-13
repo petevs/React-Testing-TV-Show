63d62b9db41c7bbd5adead7de26a6e8bcbfb14e5
Pete VS <pete_vs88@me.com>
2021-01-13 18:43:46 -0500

    added episode list tests

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -9,3 +9,11 @@ const episodes = showData.data._embedded.episodes
 test("renders without errors", () => {
     render(<Episodes episodes={episodes}/>)
 })
+
+test("shows list of episode names", () => {
+    render(<Episodes episodes={episodes}/>)
+    const episodeList = screen.queryAllByTestId('episode')
+    expect(episodeList[0]).toHaveTextContent(/chapter one/i)
+    expect(episodeList[0]).toHaveTextContent(/episode 1/i)
+    expect(episodeList[0]).toHaveTextContent(/60 minutes/i)
+}) 
